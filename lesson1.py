cd5444e1228c637d29b8fca50dd6d0581365e851
Nartey <rn185117@ncratleos.com>
2024-07-11 18:53:39 +0000

    lesson1.py

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+print ("Techsity University | Student Details")
+
+name = "Razak Nartey"
+print(name)
+
+regnumb = "RN42U232123"
+print(regnumb)
+
+dob = "31/7/1993"
+print(dob)
+
+address = "Community 25"
+print(address)
+
+city = "Tema"
+print (city)
+
+phonenumb ="0540379690"
+print(phonenumb)
+
+score_in_subject1 = 90
+print(score_in_subject1)
+
+score_in_subject2 = 98
+print(score_in_subject2)
+
+average_score = (score_in_subject1 + score_in_subject2)/2
+print(average_score)
